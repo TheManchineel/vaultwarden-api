fabca5bd86e47cbe39d837a1528297cfec4e7dc1
Michael Telatynski <7t3chguy@gmail.com>
2024-01-06 20:17:17 +0000

    Update api_routes.py

--- vaultwarden_api/api_routes.py
+++ vaultwarden_api/api_routes.py
@@ -12,7 +12,7 @@ async def status(api_key: Annotated[str, Depends(get_api_key)]) -> dict:
         users = vw_api.get_users()
         organizations = vw_api.get_organizations()
     except Exception as e:
-        print(e)
+        print(e)  # print exception
         raise HTTPException(status_code=500, detail="Vaultwarden API error")
 
     return {
